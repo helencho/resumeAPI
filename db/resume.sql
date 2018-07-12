188f2800e10dbffa42ab01189366de2678ad0288
helencho <helencho@ac.c4q.nyc>
2018-07-12 15:32:16 -0400

    seed database

--- db/resume.sql
+++ db/resume.sql
@@ -17,6 +17,7 @@ DROP TABLE experience;
 CREATE TABLE experience (
     id SERIAL UNIQUE,
     name VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
     city VARCHAR,
     state VARCHAR NOT NULL,
@@ -40,9 +41,12 @@ CREATE TABLE projects (
     id SERIAL UNIQUE,
     experience_id INTEGER,
     name VARCHAR NOT NULL,
+    role VARCHAR,
     description VARCHAR NOT NULL,
     start_date VARCHAR,
     end_date VARCHAR,
+    source VARCHAR,
+    live VARCHAR,
     PRIMARY KEY (id),
     FOREIGN KEY (experience_id) REFERENCES experience(id) 
 );
@@ -57,34 +61,16 @@ CREATE TABLE skills (
 
 DROP TABLE tech_stacks;
 CREATE TABLE tech_stacks (
+    project_id INTEGER,
     skill_id INTEGER,
-    project_id INTEGER,
-    experience_id INTEGER,
-    FOREIGN KEY (skill_id) REFERENCES skills(id),
     FOREIGN KEY (project_id) REFERENCES projects(id),
-    FOREIGN KEY (experience_id) REFERENCES experience(id)
-);
-
-DROP TABLE project_source;
-CREATE TABLE project_source (
-    id SERIAL UNIQUE,
-    project_id INTEGER,
-    url VARCHAR NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (project_id) REFERENCES projects(id) 
-);
-
-DROP TABLE project_live;
-CREATE TABLE project_live (
-    id SERIAL UNIQUE, 
-    project_id INTEGER,
-    url VARCHAR NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (project_id) REFERENCES projects(id)
+    FOREIGN KEY (skill_id) REFERENCES skills(id)
 );
 
 DROP TABLE contact;
 CREATE TABLE contact (
+    first_name VARCHAR,
+    last_name VARCHAR,
     email VARCHAR,
     phone VARCHAR,
     linkedin VARCHAR,
@@ -96,58 +82,62 @@ CREATE TABLE contact (
 );
 
 -- Seed data 
--- INSERT INTO education (name, start_date, end_date, city, state)
--- VALUES 
---     ('University at Albany', '2009', '2014', 'Albany', 'NY')
--- ;
+INSERT INTO education (name, start_date, end_date, city, state)
+VALUES 
+    ('University at Albany', '2009', '2014', 'Albany', 'NY'),
+    ('Herricks High School', null, '2009', 'New Hyde Park', 'NY')
+;
 
--- INSERT INTO experience (name, description, city, state, start_end, end_date)
--- VALUES 
---     ('Coalition for Queens (C4Q)', 'Gained hands on experience building dynamic web applications using Postgres, Express, React and Node.js', 'Long Island City', 'NY', '2017-09', '2018-06')
--- ;
+INSERT INTO experience (name, title, description, city, state, start_date, end_date)
+VALUES 
+    ('Coalition for Queens (C4Q)', 'Full Stack Web Development Fellow', 'Gained hands on experience building dynamic web applications using Postgres, Express, React and Node.js', 'Long Island City', 'NY', '2017-09', '2018-06'),
+    ('Choe & Oh, P.C.', 'Legal Assistant', 'Draft contracts, make bill payments, direct phone calls', 'New York', 'NY', '2015-10', '2017-09')
+;
 
--- INSERT INTO degrees (education_id, level, name)
--- VALUES 
---     (1, 'Bachelor of Arts', 'English Literature')
--- ;
+INSERT INTO degrees (education_id, level, name)
+VALUES 
+    (1, 'Bachelor of Arts', 'English Literature')
+;
 
--- INSERT INTO projects (experience_id, name, description, start_date, end_date)
--- VALUES 
---     (1, 'Smood', 'Track your moods', '2018-05', '2018-06')
--- ;
+INSERT INTO projects (experience_id, name, role, description, start_date, end_date, source, live)
+VALUES 
+    (1, 'Smood', null, 'Track your moods', '2018-05', '2018-06', 'https://github.com/helencho/smood', 'https://smood.herokuapp.com'),
+    (1, 'Elevate', 'Design Lead', 'Organize and manage job applications', '2018-04', '2018-05', null, null),
+    (1, 'Sinistagram', null, 'Instagram inspired photo sharing app for the sinister', null, null, null, null)
+;
 
--- INSERT INTO skills (name)
--- VALUES 
---     ('JavaScript'),
---     ('React'),
---     ('Redux'),
---     ('Node.js'),
---     ('Express'),
---     ('Postgres'),
---     ('SQL'),
---     ('HTML'),
---     ('CSS'),
---     ('jQuery'),
---     ('RESTful API'),
---     ('Git'),
---     ('GitHub'),
---     ('Heroku'),
---     ('Illustrator'),
---     ('Hootsuite'),
---     ('QuickBooks')
--- ;
+INSERT INTO skills (name, description)
+VALUES 
+    ('JavaScript', 'Javascript'),
+    ('React', 'JavaScript framework'),
+    ('Redux', 'Flux framework'),
+    ('Node.js', 'Runtime environment'),
+    ('Express', 'Server-side web framework'),
+    ('Postgres', 'Database management'),
+    ('SQL', 'Structured Query Language'),
+    ('HTML', 'Templates'),
+    ('CSS', 'Styles'),
+    ('jQuery', 'Stylish JavaScript'),
+    ('RESTful API', 'Understanding REST architecture of the web'),
+    ('Git', 'Version control'),
+    ('GitHub', 'Version control online'),
+    ('Heroku', 'Hosting and deploying applications'),
+    ('Illustrator', 'Vector software'),
+    ('Hootsuite', 'Social media manager'),
+    ('QuickBooks', 'Accounting system')
+;
 
--- INSERT INTO project_source (project_id, url) 
--- VALUES 
---     (1, 'https://github.com/helencho/smood')
--- ;
+INSERT INTO tech_stacks (project_id, skill_id) 
+VALUES 
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (2, 1),
+    (3, 1)
+;
 
--- INSERT INTO project_live (project_id, url)
--- VALUES 
---     (1, 'https://smood.herokuapp.com')
--- ;
-
--- INSERT INTO contact (email, phone, linkedin, twitter, github, pinterest, facebook, instagram) 
--- VALUES 
---     ('helencho@ac.c4q.nyc', '5162447969', 'https://www.linkedin.com/in/haeyoungcho/', 'https://twitter.com/hellohelencho', 'https://github.com/helencho', null, null, null)
--- ;
+INSERT INTO contact (first_name, last_name, email, phone, linkedin, twitter, github, pinterest, facebook, instagram) 
+VALUES 
+    ('Helen', 'Cho', 'helencho@ac.c4q.nyc', '5162447969', 'https://www.linkedin.com/in/haeyoungcho/', 'https://twitter.com/hellohelencho', 'https://github.com/helencho', null, null, null)
+;
