9e5bbff3bd8a6ab5858caa3bf0d8a28d68fdd89d
helencho <helencho@ac.c4q.nyc>
2018-07-12 15:11:14 -0400

    database schemas

--- db/resume.sql
+++ db/resume.sql
@@ -6,8 +6,8 @@ DROP TABLE education;
 CREATE TABLE education (
     id SERIAL UNIQUE, 
     name VARCHAR NOT NULL, 
-    start_date DATE,
-    end_date DATE, 
+    start_date VARCHAR,
+    end_date VARCHAR, 
     city VARCHAR,
     state VARCHAR NOT NULL,
     PRIMARY KEY (id) 
@@ -20,8 +20,8 @@ CREATE TABLE experience (
     description VARCHAR NOT NULL,
     city VARCHAR,
     state VARCHAR NOT NULL,
-    start_date DATE,
-    end_date DATE, 
+    start_date VARCHAR,
+    end_date VARCHAR, 
     PRIMARY KEY (id) 
 );
 
@@ -29,6 +29,7 @@ DROP TABLE degrees;
 CREATE TABLE degrees (
     id SERIAL UNIQUE, 
     education_id INTEGER,
+    level VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (education_id) REFERENCES education(id) 
@@ -40,8 +41,8 @@ CREATE TABLE projects (
     experience_id INTEGER,
     name VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
-    start_date DATE,
-    end_date DATE,
+    start_date VARCHAR,
+    end_date VARCHAR,
     PRIMARY KEY (id),
     FOREIGN KEY (experience_id) REFERENCES experience(id) 
 );
@@ -49,22 +50,37 @@ CREATE TABLE projects (
 DROP TABLE skills;
 CREATE TABLE skills (
     id SERIAL UNIQUE, 
-    project_id INTEGER,
-    experience_id INTEGER,
     name VARCHAR NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (project_id) REFERENCES projects(id),
-    FOREIGN KEY (experience_id) REFERENCES experience(id) 
+    description VARCHAR,
+    PRIMARY KEY (id)
 );
 
-DROP TABLE project_links;
-CREATE TABLE project_links (
+DROP TABLE tech_stacks;
+CREATE TABLE tech_stacks (
+    skill_id INTEGER,
+    project_id INTEGER,
+    experience_id INTEGER,
+    FOREIGN KEY (skill_id) REFERENCES skills(id),
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (experience_id) REFERENCES experience(id)
+);
+
+DROP TABLE project_source;
+CREATE TABLE project_source (
     id SERIAL UNIQUE,
     project_id INTEGER,
     url VARCHAR NOT NULL,
-    type VARCHAR NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (project_id) REFERENCES projects(id) 
+);
+
+DROP TABLE project_live;
+CREATE TABLE project_live (
+    id SERIAL UNIQUE, 
+    project_id INTEGER,
+    url VARCHAR NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (project_id) REFERENCES projects(id)
 );
 
 DROP TABLE contact;
@@ -78,3 +94,60 @@ CREATE TABLE contact (
     facebook VARCHAR,
     instagram VARCHAR
 );
+
+-- Seed data 
+-- INSERT INTO education (name, start_date, end_date, city, state)
+-- VALUES 
+--     ('University at Albany', '2009', '2014', 'Albany', 'NY')
+-- ;
+
+-- INSERT INTO experience (name, description, city, state, start_end, end_date)
+-- VALUES 
+--     ('Coalition for Queens (C4Q)', 'Gained hands on experience building dynamic web applications using Postgres, Express, React and Node.js', 'Long Island City', 'NY', '2017-09', '2018-06')
+-- ;
+
+-- INSERT INTO degrees (education_id, level, name)
+-- VALUES 
+--     (1, 'Bachelor of Arts', 'English Literature')
+-- ;
+
+-- INSERT INTO projects (experience_id, name, description, start_date, end_date)
+-- VALUES 
+--     (1, 'Smood', 'Track your moods', '2018-05', '2018-06')
+-- ;
+
+-- INSERT INTO skills (name)
+-- VALUES 
+--     ('JavaScript'),
+--     ('React'),
+--     ('Redux'),
+--     ('Node.js'),
+--     ('Express'),
+--     ('Postgres'),
+--     ('SQL'),
+--     ('HTML'),
+--     ('CSS'),
+--     ('jQuery'),
+--     ('RESTful API'),
+--     ('Git'),
+--     ('GitHub'),
+--     ('Heroku'),
+--     ('Illustrator'),
+--     ('Hootsuite'),
+--     ('QuickBooks')
+-- ;
+
+-- INSERT INTO project_source (project_id, url) 
+-- VALUES 
+--     (1, 'https://github.com/helencho/smood')
+-- ;
+
+-- INSERT INTO project_live (project_id, url)
+-- VALUES 
+--     (1, 'https://smood.herokuapp.com')
+-- ;
+
+-- INSERT INTO contact (email, phone, linkedin, twitter, github, pinterest, facebook, instagram) 
+-- VALUES 
+--     ('helencho@ac.c4q.nyc', '5162447969', 'https://www.linkedin.com/in/haeyoungcho/', 'https://twitter.com/hellohelencho', 'https://github.com/helencho', null, null, null)
+-- ;
