f2e6c8113d616629506f941e22d8e7a3c62873fb
helencho <helencho@ac.c4q.nyc>
2018-07-16 18:44:42 -0400

    finally wrote a sql query that grabs rows and columns the way I imagined it to be

--- db/queries/projects.js
+++ db/queries/projects.js
@@ -3,7 +3,7 @@ const db = require('../index')
 // GET : /projects 
 const getAll = (req, res) => {
     db
-        .any('SELECT * FROM projects LEFT JOIN tech_stacks ON projects.id = tech_stacks.project_id LEFT JOIN skills ON tech_stacks.skill_id = skills.id;')
+        .any("SELECT projects.id, projects.project_name, projects.description, projects.end_date, array_agg(skill) AS skills, (SELECT json_build_object('source', source, 'live', live) FROM projects c WHERE projects.id = c.id) AS links FROM projects LEFT JOIN tech_stacks ON projects.id = tech_stacks.project_id LEFT JOIN skills ON tech_stacks.skill_id = skills.id GROUP BY projects.id ORDER BY projects.end_date DESC;")
         .then(data => {
             res.status(200).json({
                 status: 'Success',
