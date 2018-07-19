bc70ad379061b2e60d9eb95c0ec8ef6e73e2b9c4
helencho <helencho@ac.c4q.nyc>
2018-07-19 09:57:14 -0400

    query for single experience

--- db/queries/experience.js
+++ db/queries/experience.js
@@ -19,6 +19,27 @@ const getAll = (req, res) => {
         })
 }
 
+const getOne = (req, res) => {
+    db
+        .one("SELECT * FROM experience WHERE id=${id}", {
+            id: req.params.id
+        })
+        .then(data => {
+            res.status(200).json({
+                status: 'Success',
+                message: 'Retrieved one experience',
+                data: data
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message,
+                error: {}
+            })
+        })
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getOne
 }
