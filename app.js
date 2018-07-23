3c0cac47bd512baec642d6e088cfcbf711c66f5c
helencho <helencho@ac.c4q.nyc>
2018-07-23 17:06:40 -0400

    prettify json output

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.set('json spaces', 4); 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
